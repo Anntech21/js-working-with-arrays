86f33c5957593b51a0d2d64524e0ea8ffc1aa09d
Ann Tech <anntech21@gmail.com>
2023-08-14 14:00:26 -0700

    ex9

--- scripts.js
+++ scripts.js
@@ -9,11 +9,6 @@
 
 /********************** Exercise 2 **********************/
 // Create an array of books to be sold in an online store. 
-let book1 = "abc";
-let book2 = "def";
-let book3 = "ghi";
-let book4 = "jkl";
-let book5 = "mno";
 
 let books = ["abc", "def", "ghi", "jkl", "mno"];
 console.log("books");
@@ -35,12 +30,15 @@ shopping[2] = "vegetables";
 // Log the value at index 2. Note that it is NOT the second value! Why is that?
 //Answer: because index starts from 0
 
+console.log(shopping[(2+4)/3]);
+
 // Log the last value WITHOUT using a set number. That is, do NOT write: `shopping[4]`. You can use the `.length` to figure out what the last index is!
-shopping[shopping.length-1];
+console.log(shopping[shopping.length-1]);
+
 // Now write the same code in the square brackets, using the `.length` property to calculate the last index, only with the `gems` array below instead of `shopping`. The solution should give you the last value in ANY array!
 
 const gems = ["ruby", "saphhire", "pearl", "steven", "amethyst", "garnet", "lapis lazuli", "peridot"]
-gems[gems.length-1];
+console.log (gems[gems.length-1]);
 
 
 /********************** Exercise 4 **********************/
@@ -70,26 +68,32 @@ const colors = ["red", "green", "blue", "yellow"];
 
 // Use the slice method on the above array to produce the following array: ["red", "green", "blue"]
 
+let newColors = colors.slice(0,3);
+console.log(newColors);
+
 // Now use the slice method to produce the following array: ["green", "blue", "yellow"]
+let newColors1 = colors.slice(1,4);
 
 // Next use the slice method to produce the following array: ["red", "green"]
 
-
-
+let newColors2 = colors.slice(0,2);
 /********************** Exercise 6 **********************/
 
 const numbers = [5, 1, 8, 3, 2, 7, 4, 6, 9];
 
 // Sort the array in ascending order
-
-
-
+console.log(numbers.sort());
 
 /********************** Exercise 7 **********************/
 // The following array contains data about a user. How would you log the string 'Python' to the console?
 const userMember = ["Dava Sorbel", 795, ['Python', 'JavaScript', 'Ruby']];
 
+let result1 = userMember.join('Python', 'JavaScript', 'Ruby');
+
 // How would you use that array to find the letter J? Remember that you can use indexes with arrays AND strings!
+
+console.log(result1.split('avaScript,'));
+
 
 
 /********************** Exercise 8 **********************/
@@ -97,18 +101,23 @@ const userMember = ["Dava Sorbel", 795, ['Python', 'JavaScript', 'Ruby']];
 const cityString = "Detroit,Atlanta,Birmingham,New Haven,Dallas,San Diego,Portland";
 
 // Now let's split the array at each comma. How would you create an array with each of the cities as a separate value?
-
+let cityArray = cityString.split(",");
+console.log(cityArray);
+//cityString.split('Detroit,Atlanta,Birmingham,New Haven,Dallas,San Diego,Portlan');
 
 // What is the length of the new array?
-
+//62
+console.log(cityArray.length);
 
 // Log the array to the console
 
-
+console.log(cityArray);
 // How would you log the last city in the array to the console?
 
+console.log(cityArray[6]);
 
 // Using the .join() method, how would you turn the list of cities back into a string with spaces after the commas?
+console.log(cityArray.join(", "));
 
 
 /********************** Exercise 9 **********************/
@@ -117,16 +126,17 @@ const cityString = "Detroit,Atlanta,Birmingham,New Haven,Dallas,San Diego,Portla
 let myString = 'Ryu+Ken+Chun-Li+Cammy+Guile+Sakura+Sagat+Juri';
 
 // Convert the string into an array, removing the + characters in the process. Save the result in a variable called myArray.
-
+let myArray = myString.split('+');
 
 // Store the length of the array in a variable called arrayLength.
-
+let arrayLength = (myArray.length);
 
 // Store the last item in the array in a variable called lastItem.
-
+let lastItem = (myArray.slice[8]+ ("lastItem"));
 
 // Remove the last item in the array.
-
-
+console.log(myArray.push());
 
 // Add two new names to the end of the array.
+console.log(myArray.unshift("Neha","Appu"));
+
